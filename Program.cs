8f2364c9a817b89b9f3908c272410b56bb279866
Christopher Krauss <christopherjosekrauss@gmail.com>
2023-10-08 08:55:04 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+void main()
+{
+    Console.WriteLine("Bem vindo ao jogo NumberGuesser");
+    Console.WriteLine("O Numero pode ser entre 1 e 100\n");
+    int NumberToGuess= AleatoryNumberGenerator();
+    //Console.WriteLine(NumberToGuess);
+    ReceivePlayerNumber(NumberToGuess);
+
+}
+
+int AleatoryNumberGenerator()
+{
+    Random NumAleatory = new();
+    int valorInteiro = NumAleatory.Next(1,101);
+    //Console.WriteLine(valorInteiro);
+    return valorInteiro;
+ }
+
+int ReceivePlayerNumber(int AleatoryNumber)
+{
+    Console.WriteLine("terceira função:\n");
+    Console.WriteLine($"{AleatoryNumber}");
+    return 0;
+}
+
+
+main();
